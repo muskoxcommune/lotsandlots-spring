c9096b4bcf69b0fcbfaf5209503aea056e27adc5
muskoxcommune <muskoxcommune@gmail.com>
2023-06-03 14:46:06 -0700

    Generated templated

---
+++ src/main/java/org/example/lotsandlots/LotsandlotsApplication.java
@@ -0,0 +1,13 @@
+package org.example.lotsandlots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotsandlotsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LotsandlotsApplication.class, args);
+	}
+
+}
